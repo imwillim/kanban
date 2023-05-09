b9374429a16d29607de39a32103c390ccd8bfc56
imwillim <ankhkien@gmail.com>
2023-05-09 21:56:20 +0700

    main: Init a project using Spring Boot.

---
+++ src/main/java/com/project/kanban/KanbanApplication.java
@@ -0,0 +1,16 @@
+package com.project.kanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class KanbanApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(KanbanApplication.class, args);
+
+    }
+
+}
